382e37a4dfbc4cc22e01d2bc7d7f9a2909b9d919
Abdulkarim <abdyabkor@gmail.com>
2025-06-12 19:29:06 +0300

    add textarea components

---
+++ src/components/TextArea.tsx
@@ -0,0 +1,16 @@
+import type { FC, TextareaHTMLAttributes } from "react"
+import cn from "../utils/cn"
+
+
+interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> { }
+
+const TextArea: FC<TextAreaProps> = ({ className, ...props }) => {
+    return (
+        <textarea
+            className={cn("w-full text-white bg-gray-400 p-1", className)}
+            {...props}
+        />
+    )
+}
+
+export default TextArea
