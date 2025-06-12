f00427eb680fa94f6d1a217b432c6749fdea227d
Abdulkarim <abdyabkor@gmail.com>
2025-06-12 19:35:24 +0300

    edit signup and login pages

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,21 +1,60 @@
+import { Link } from "react-router";
+import Button from "../../components/Button";
+import Form from "../../components/Form"
+import Input from "../../components/Input";
 import supabase from "../../utils/supabase"
 
 const Login = () => {
 
-    const login = async () => {
-        await supabase.auth.signInWithPassword({
-            email: 'example@email.com',
-            password: 'example-password',
-        })
+    const login = async (formData: FormData) => {
+        const email = formData.get("email")?.toString();
+        const password = formData.get("password")?.toString();
+        if (!email || !password)
+            return;
+        const { error } = await supabase.auth.signInWithPassword({
+            email,
+            password,
+        });
+        if (error)
+            console.error(error.message);
     }
 
     return (
-        <div>
-            <button
-                onClick={login}
+        <div
+            className="bg-slate-950 w-screen h-screen flex justify-center items-center"
+        >
+            <Form
+                className="bg-slate-900 p-3 rounded-2xl w-70 text-white"
+                action={login}
             >
-                login
-            </button>
+                <Input
+                    name="email"
+                    type="email"
+                    placeholder="email"
+                />
+                <Input
+                    name="password"
+                    type="text"
+                    placeholder="password"
+                />
+                <Button
+                    className="bg-sky-400 disabled:bg-sky-800 py-1"
+                    type="submit"
+                >
+                    Login
+                </Button>
+                <p
+                    className="text-white text-sm"
+                >
+                    if you don't have account
+                    <Link
+                        className="text-blue-500 pl-1"
+                        to={"/signup"}
+                    >
+                        signup
+                    </Link>
+                </p>
+            </Form>
         </div>
     )
 }
--- src/pages/signup/Signup.tsx
+++ src/pages/signup/Signup.tsx
@@ -1,26 +1,60 @@
+import { Link } from "react-router";
+import Button from "../../components/Button";
+import Form from "../../components/Form";
+import Input from "../../components/Input";
 import supabase from "../../utils/supabase"
 
 const Signup = () => {
 
-    const signup = async () => {
-        try {
-            await supabase.auth.signUp({
-                email: 'example@email.com',
-                password: 'example-password',
-            })
-        } catch (error) {
-            if (error instanceof Error)
-                console.log(error.message);
-        }
+    const signup = async (formData: FormData) => {
+        const email = formData.get("email")?.toString();
+        const password = formData.get("password")?.toString();
+        if (!email || !password)
+            return;
+        const { error } = await supabase.auth.signUp({
+            email,
+            password,
+        })
+        if (error)
+            console.error(error.message);
     }
 
     return (
-        <div>
-            <button
-                onClick={signup}
+        <div
+            className="bg-slate-950 w-screen h-screen flex justify-center items-center"
+        >
+            <Form
+                className="bg-slate-900 p-3 rounded-2xl w-70 text-white"
+                action={signup}
             >
-                signUp
-            </button>
+                <Input
+                    name="email"
+                    type="email"
+                    placeholder="email"
+                />
+                <Input
+                    name="password"
+                    type="text"
+                    placeholder="password"
+                />
+                <Button
+                    className="bg-sky-400 disabled:bg-sky-800 w-full py-1"
+                    type="submit"
+                >
+                    Signup
+                </Button>
+                <p
+                    className="text-white text-sm"
+                >
+                    if you have account
+                    <Link
+                        className="text-blue-500 pl-1"
+                        to={"/login"}
+                    >
+                        login
+                    </Link>
+                </p>
+            </Form>
         </div>
     )
 }
